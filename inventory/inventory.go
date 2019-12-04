a37e6d8ef879d547ec84ec89b5e40249c7b3fc4b
Mehmet Gürdal <mehmet.gurdal@metglobal.com>
2019-12-04 20:07:52 +0300

    Add docstrings to inventory methods

--- inventory/inventory.go
+++ inventory/inventory.go
@@ -4,20 +4,24 @@ import "errors"
 
 type Inventory struct {
 	Items map[string]*Item
-	q     int
+	Total int
 }
 
+// Add upserts the target item to the inventory.
 func (inv *Inventory) Add(item *Item) {
 	items := inv.Items
 	if inventoryItem, contains := items[item.Name]; contains {
 		inventoryItem.Quantity += item.Quantity
-		inv.q += item.Quantity
+		inv.Total += item.Quantity
 	} else {
 		items[item.Name] = item
 	}
 }
 
-// Check
+// Check compares the consumption requirements with the user inventory
+// in order to check the productability of a unit of the target item.
+// TODO: we might want to move this method to factory since the production
+// task is only related to the factory.
 func (inv *Inventory) Check(consumptions map[string]*Item) (ok bool) {
 	ok = true
 	items := inv.Items
@@ -36,7 +40,7 @@ func (inv *Inventory) Check(consumptions map[string]*Item) (ok bool) {
 	return
 }
 
-// Consume
+// Consume reduces the target item's quantity if its in the inventory.
 func (inv *Inventory) Consume(item Item) (err error) {
 	items := inv.Items
 	inventoryItem, exists := items[item.Name]
